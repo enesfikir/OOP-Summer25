87d8417b1ab1d071221588c53076fccba84abfce
enesfikir <76658692+enesfikir@users.noreply.github.com>
2025-03-13 11:44:01 +0000

    Add student class

---
+++ student_details_structures.py
@@ -0,0 +1,48 @@
+student1={
+    "first_name": "Enes",
+    "last_name": "Fikir",
+    "index_number": "35197",
+    "nationality": "Turkish",
+    "starting_date": "01/10/2024",
+    "courses": "Basics of Cs"
+
+}
+print(student1["first_name"])
+print(student1["last_name"])
+print(student1["index_number"])
+print(student1["nationality"])
+print(student1["starting_date"])
+print(student1["courses"])
+
+student2={
+    "first_name": "Fenasi",
+    "last_name": "Kerim",
+    "index_number": "35250",
+    "nationality": "Turkish",
+    "starting_date": "01/10/2024",
+    "courses": "Basics of Cs"
+
+}
+print(student2["first_name"])
+print(student2["last_name"])
+print(student2["index_number"])
+print(student2["nationality"])
+print(student2["starting_date"])
+print(student2["courses"])
+
+student3={
+    "first_name": "Dursun",
+    "last_name": "Sanakoysun",
+    "index_number": "35210",
+    "nationality": "Turkish",
+    "starting_date": "01/10/2024",
+    "courses": "Basics of Cs"
+
+}
+print(student3["first_name"])
+print(student3["last_name"])
+print(student3["index_number"])
+print(student3["nationality"])
+print(student3["starting_date"])
+print(student3["courses"])
+
